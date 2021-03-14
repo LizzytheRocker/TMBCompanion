cb03c732093ee694b22876bdc47b9a9dec50d498
nexo <as8md@mst.edu>
2021-03-13 23:06:04 -0600

    Initial character submission button

--- src/CharSelect.js
+++ src/CharSelect.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Character from './Character.js';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import { TextBoxComponent } from '@syncfusion/ej2-react-inputs';
 // <TextBoxComponent placeholder="Outlined" cssClass="e-outline" floatLabelType="Auto" />
 
@@ -9,7 +9,32 @@ import PropTypes from 'prop-types';
 class CharSelect extends React.Component {
 	constructor() {
 		super();
-		this.state = new Character
+		this.state = { char: new Character }
+	}
+
+	submitChar() {
+		var name = document.getElementByID('CharSelectName').nodeValue;
+		this.state.char.name = name;
+		var hp = document.getElementById('CharSelectBaseHP').nodeValue;
+		var dex = document.getElementById('CharSelectBaseDEX').nodeValue;
+		var atk = document.getElementById('CharSelectBaseATK').nodeValue;
+		var def = document.getElementById('CharSelectBaseDEF').nodeValue;
+		this.state.char.stats_base = [hp, dex, atk, def];
+		var dice_acq = document.getElementById('CharSelectDiceAcq').nodeValue;
+		this.state.char.dice_acq = dice_acq;
+		var innate = document.getElementById('CharSelectInnate').nodeValue;
+		this.state.char.innate = innate;
+		var curr_hp = document.getElementById('CharSelectCurrHP').nodeValue;
+		this.state.char.curr_hp = curr_hp;
+		var loot = document.getElementById('CharSelectLoot').nodeValue;
+		this.state.char.loot = loot;
+		var notes = document.getElementById('CharSelectNotes').nodeValue;
+		this.state.char.player_notes = notes;
+		var locked = document.getElementById('CharSelectLockedSlots').nodeValue;
+		this.state.char.locked_slots = locked;
+		var scars = document.getElementById('CharSelectScars').nodeValue;
+		this.state.char.scars = scars;
+		console.log(JSON.stringify(this.state.char))
 	}
 
 	render () {
@@ -51,12 +76,12 @@ class CharSelect extends React.Component {
 					<h2>Innate</h2>
 					<p>Check if innate + 1 has been reached</p>
 					<input
-						id='CharSelectBaseInnate'
+						id='CharSelectInnate'
 						type='checkbox'
 					/>
 					<h2>Current HP</h2>
 					<input
-						id='CharSelectBaseCurrHP'
+						id='CharSelectCurrHP'
 						type='number'
 					/>
 					<h2>Loot</h2>
@@ -81,12 +106,22 @@ class CharSelect extends React.Component {
 						id='CharSelectScars'
 						type='text'
 					/>
+					<input
+						id='CharSelectSubmit'
+						type='submit'
+						value='Submit'
+						onClick='submitChar()'
+					/>
+					{/* <script type="text/javascript">
+						function submitChar() {
+							var name = document.getElementByID('CharSelectName').value;
+							this.state.char.#name = name;
+						}
+					</script> */}
 				</form>
 			</div>
 		)
 	}
 }
-
-
 
 export default CharSelect
