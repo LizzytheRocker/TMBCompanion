145ffe6d442b837ef24cad374f7da6a82740d6dc
nexo <as8md@mst.edu>
2021-03-18 11:24:16 -0500

    Removed unused commented code

--- src/CharInfoButton.js
+++ src/CharInfoButton.js
@@ -16,23 +16,9 @@ class CharInfoButton extends React.Component {
 	render () {
 		return (
 			<div className='CharInfoButton'>
-				{/* <h2>Character Selection</h2>
-				<p>Select the character that you want to add to the campaign.</p> */}
 				<div className="dropdown">
 					<button className="dropbtn" onClick={this._showContent.bind(null, this.state.showContent ? false : true)}>Character Info</button>
 					{ this.state.showContent && (<CharInfo />) }
-					
-					{/* <div className="dropdown-content">
-						<a href="#">Boomer</a>
-						<a href="#">Duster</a>
-						<a href="#">Ghillie</a>
-						<a href="#">Nugget</a>
-						<a href="#">Patches</a>
-						<a href="#">Picket</a>
-						<a href="#">Stanza</a>
-						<a href="#">Tantrum</a>
-						<a href="#">Tink</a>
-					</div> */}
 				</div>
 			</div>
 		)
